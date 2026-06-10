2d1d7a74f456d286e4e795977d75f7c28a7c4cef
Felipe Amais <felipeoamais@gmail.com>
2026-06-10 20:37:23 -0300

    refactor: implementa DTOs para despesas e corrige atualizacao

--- src/main/java/com/felipe/despesas/model/Despesa.java
+++ src/main/java/com/felipe/despesas/model/Despesa.java
@@ -2,6 +2,7 @@ package com.felipe.despesas.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "despesas")
 @Data
+@NoArgsConstructor
 public class Despesa {
     
     @Id
@@ -26,5 +28,13 @@ public class Despesa {
 
     @ManyToOne
     private Usuario usuario;
+
+    public Despesa(Long id, String descricao, BigDecimal valor, LocalDate data, Categoria categoria) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+        this.categoria = categoria;
+    }
 
 }
