15115d697095b0043d7a393a7b0110fa4b902d36
Felipe Amais <felipeoamais@gmail.com>
2026-06-14 15:11:51 -0300

    feat: adiciona Bean Validation nos DTOs de despesa

--- src/main/java/com/felipe/despesas/dto/DespesaRequest.java
+++ src/main/java/com/felipe/despesas/dto/DespesaRequest.java
@@ -1,5 +1,9 @@
 package com.felipe.despesas.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,11 +12,17 @@ import java.time.LocalDate;
 @Data
 public class DespesaRequest {
 
+    @NotBlank
     private String descricao;
 
+    @NotNull
+    @Positive
     private BigDecimal valor;
 
+    @NotNull
+    @PastOrPresent
     private LocalDate data;
 
+    @NotNull
     private Long categoriaId;
 }
